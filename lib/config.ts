be648fd16f7d6120627b3acf7defd3fdf8042dbd
asuma-labs <asuma.my.id@gmail.com>
2026-06-09 05:20:43 +0700

    Create config.ts

---
+++ lib/config.ts
@@ -0,0 +1,5 @@
+// lib/config.ts
+// File ini bisa dipakai di Server Component maupun Client Component
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://bot.asuma.my.id';
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://demo.asuma.my.id';
