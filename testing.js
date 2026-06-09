800dbedd10453526dd918eb18dd765e246403570
asuma-labs <asuma.my.id@gmail.com>
2026-06-09 23:03:18 +0700

    Create testing.js

---
+++ testing.js
@@ -0,0 +1,12 @@
+fetch('https://bot.asuma.my.id/api/auth/request-otp', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ phone: '628123456789' })
+}).then(async res => {
+  console.log('Status:', res.status);
+  console.log('Content-Type:', res.headers.get('content-type'));
+  const text = await res.text();
+  console.log('Body:', text.substring(0, 100));
+}).catch(console.error);
