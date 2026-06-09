3087f6c51fd2497446ef007f35e4d0469e81cdf9
asuma-labs <asuma.my.id@gmail.com>
2026-06-09 12:57:33 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,32 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "asuma.my.id",
-      },
-      {
-        protocol: "https",
-        hostname: "*.asuma.my.id",
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       },
     ],
+  },
+  output: 'standalone',
+  transpilePackages: ['motion'],
+  webpack: (config, {dev}) => {
+    if (dev && process.env.DISABLE_HMR === 'true') {
+      config.watchOptions = {
+        ignored: /.*/,
+      };
+    }
+    return config;
   },
 };
 
