035a471092d2748b7dac0c483ea155a23f7125dc
asuma-labs <asuma.my.id@gmail.com>
2026-06-16 11:48:52 +0700

    Create template.tsx

---
+++ app/template.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { motion } from 'motion/react';
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 12 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4, ease: [0.215, 0.61, 0.355, 1] }}
+      className="flex-grow flex flex-col w-full"
+    >
+      {children}
+    </motion.div>
+  );
+}
